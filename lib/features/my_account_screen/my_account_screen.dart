2c2c522787d42774f11451fb4460ce78a2187d69
Islam Sarsenbek <134628770+joe-isya@users.noreply.github.com>
2025-08-08 09:38:49 +0500

    added bottomnav bar

--- lib/features/my_account_screen/my_account_screen.dart
+++ lib/features/my_account_screen/my_account_screen.dart
@@ -1,7 +1,7 @@
-import 'package:flex_travel_sim/constants/app_colors.dart';
 import 'package:flex_travel_sim/core/localization/app_localizations.dart';
 import 'package:flex_travel_sim/core/styles/flex_typography.dart';
 import 'package:flex_travel_sim/features/my_account_screen/widgets/account_widget.dart';
+import 'package:flex_travel_sim/features/my_account_screen/widgets/log_out_widget.dart';
 import 'package:flex_travel_sim/gen/assets.gen.dart';
 import 'package:flex_travel_sim/shared/widgets/localized_text.dart';
 import 'package:flex_travel_sim/utils/navigation_utils.dart';
@@ -15,6 +15,7 @@ class MyAccountScreen extends StatelessWidget {
     const horizontalPadding = EdgeInsets.symmetric(horizontal: 20);
 
     return Scaffold(
+      bottomNavigationBar: LogOutWidget(),
       backgroundColor: Colors.white,
       appBar: AppBar(backgroundColor: Colors.white, elevation: 0),
       body: SafeArea(
@@ -48,26 +49,8 @@ class MyAccountScreen extends StatelessWidget {
                 onTap: () => NavigationService.openLanguageScreen(context),
               ),
               Spacer(),
-              InkWell(
-                onTap: () => NavigationService.showLogoutDialog(context),
-                child: Container(
-                  height: 52,
-                  decoration: BoxDecoration(
-                    color: AppColors.redCircleColor,
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: Center(
-                    child: LocalizedText(
-                      AppLocalizations.logout,
-                      style: FlexTypography.paragraph.xMedium.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 30,)
+
+              SizedBox(height: 30),
             ],
           ),
         ),
