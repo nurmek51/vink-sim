024253b8b3a343f54072e1fc804536a3ab9d665a
Islam Sarsenbek <134628770+joe-isya@users.noreply.github.com>
2025-08-14 08:53:51 +0500

    added cache for digits and phone number

--- lib/features/onboarding/widgets/whatsapp_tile.dart
+++ lib/features/onboarding/widgets/whatsapp_tile.dart
@@ -22,15 +22,19 @@ import 'package:flex_travel_sim/gen/assets.gen.dart';
 import 'package:flutter_svg/svg.dart';
 
 class WhatsappTile extends StatefulWidget {
-  final void Function(String, ConfirmMethod) onNext;
+  final void Function(String, ConfirmMethod, [String?, Country?]) onNext;
   final VoidCallback appBarPop;
   final VoidCallback onEmailTap;
+  final String? initialPhoneDigits;
+  final Country? initialCountry;
 
   const WhatsappTile({
     super.key,
     required this.onNext,
     required this.appBarPop,
     required this.onEmailTap,
+    this.initialPhoneDigits,
+    this.initialCountry,
   });
 
   @override
@@ -41,6 +45,22 @@ class _WhatsappTileState extends State<WhatsappTile> {
   String _phoneDigits = '';
   String formattedPhone = '';
   Country _selectedCountry = CountryData.defaultCountry;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.initialCountry != null) {
+      _selectedCountry = widget.initialCountry!;
+    }
+    if (widget.initialPhoneDigits != null) {
+      _phoneDigits = widget.initialPhoneDigits!;
+      final phoneWithoutDialCode =
+          _phoneDigits.startsWith(_selectedCountry.dialCode)
+              ? _phoneDigits.substring(_selectedCountry.dialCode.length)
+              : _phoneDigits;
+      formattedPhone = phoneWithoutDialCode;
+    }
+  }
 
   bool get _isValidPhone {
     if (_phoneDigits.isEmpty) return false;
@@ -79,15 +99,19 @@ class _WhatsappTileState extends State<WhatsappTile> {
       ),
       body: BlocProvider<OtpAuthBloc>(
         create:
-            (context) =>
-                OtpAuthBloc(
-                  otpAuthDataSource: sl.get<OtpAuthDataSource>(),
-                  firebaseLoginUseCase: sl.get<FirebaseLoginUseCase>(),
-                ),
+            (context) => OtpAuthBloc(
+              otpAuthDataSource: sl.get<OtpAuthDataSource>(),
+              firebaseLoginUseCase: sl.get<FirebaseLoginUseCase>(),
+            ),
         child: BlocConsumer<OtpAuthBloc, OtpAuthState>(
           listener: (context, state) {
             if (state is OtpSmsSent) {
-              widget.onNext(state.phone, ConfirmMethod.byPhone);
+              widget.onNext(
+                state.phone,
+                ConfirmMethod.byPhone,
+                _phoneDigits,
+                _selectedCountry,
+              );
             } else if (state is OtpAuthError) {
               AppNotifier.error(
                 AppLocalizations.sendError,
@@ -148,6 +172,7 @@ class _WhatsappTileState extends State<WhatsappTile> {
                     onChanged: _onPhoneChanged,
                     onCountryChanged: _onCountryChanged,
                     initialCountry: _selectedCountry,
+                    initialPhone: formattedPhone,
                   ),
                   const SizedBox(height: 20),
                   RegistrationContainer(
@@ -184,7 +209,7 @@ class _WhatsappTileState extends State<WhatsappTile> {
                     color: AppColors.babyBlue,
                     iconPath: Assets.icons.emailLogo.path,
                   ),
-                  SizedBox(height: 35,),
+                  SizedBox(height: 35),
                 ],
               ),
             );
