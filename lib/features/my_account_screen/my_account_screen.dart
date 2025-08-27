05df089581b0a4a871fe5bb82f147ccdcbc1202b
Islam Sarsenbek <134628770+joe-isya@users.noreply.github.com>
2025-08-27 17:15:37 +0500

    removed purchase history amd usage

--- lib/features/my_account_screen/my_account_screen.dart
+++ lib/features/my_account_screen/my_account_screen.dart
@@ -31,26 +31,25 @@ class MyAccountScreen extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 35),
-              AccountWidget(
-                title: AppLocalizations.purchaseHistory,
-                icon: Assets.icons.purchaseHistory.path,
-                onTap: () => NavigationService.openPurchaseScreen(context),
-              ),
-              const SizedBox(height: 12),
-              AccountWidget(
-                title: AppLocalizations.trafficUsage,
-                icon: Assets.icons.trafficUsage.path,
-                onTap: () => NavigationService.openTrafficUsageScreen(context),
-              ),
-              const SizedBox(height: 12),
+              //TODO: realize after backend
+
+              // AccountWidget(
+              //   title: AppLocalizations.purchaseHistory,
+              //   icon: Assets.icons.purchaseHistory.path,
+              //   onTap: () => NavigationService.openPurchaseScreen(context),
+              // ),
+              // const SizedBox(height: 12),
+              // AccountWidget(
+              //   title: AppLocalizations.trafficUsage,
+              //   icon: Assets.icons.trafficUsage.path,
+              //   onTap: () => NavigationService.openTrafficUsageScreen(context),
+              // ),
+              // const SizedBox(height: 12),
               AccountWidget(
                 title: AppLocalizations.appLanguage,
                 icon: Assets.icons.globusWithBackground.path,
                 onTap: () => NavigationService.openLanguageScreen(context),
               ),
-              Spacer(),
-
-              SizedBox(height: 30),
             ],
           ),
         ),
